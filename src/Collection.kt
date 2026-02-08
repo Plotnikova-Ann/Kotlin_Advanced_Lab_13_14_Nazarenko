39f580b461bf85b10feab3976d8894a13a853551
Plotnikova <plotnikovanana@gmail.com>
2026-02-08 12:46:28 +0300

    Arrays in Kotlin

---
+++ src/Collection.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val solarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem[2])
+}
