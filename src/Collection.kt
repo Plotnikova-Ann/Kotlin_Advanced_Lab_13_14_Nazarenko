d46a23bb4ee55a6d3b740db7d6e88fc6a22840cb
Plotnikova <plotnikovanana@gmail.com>
2026-02-08 12:49:30 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -1,5 +1,18 @@
 fun main() {
-    val solarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
-    println(solarSystem.size)
-    println(solarSystem[2])
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    solarSystem.add("Pluto")
+    println(solarSystem.contains("Pluto"))
+    val solarSystemMoons = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystemMoons["Earth"])
+    solarSystemMoons["Pluto"] = 5
+    println(solarSystemMoons["Pluto"])
 }
